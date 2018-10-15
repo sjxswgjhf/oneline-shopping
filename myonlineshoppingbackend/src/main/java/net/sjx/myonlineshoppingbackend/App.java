08d40e3e241720fca82c721a4fefd104fa8bb8a1
sjx <jiaxinsun0715@gmail.com>
2018-10-15 13:30:53 -0400

    my project setup completed

---
+++ myonlineshoppingbackend/src/main/java/net/sjx/myonlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sjx.myonlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
