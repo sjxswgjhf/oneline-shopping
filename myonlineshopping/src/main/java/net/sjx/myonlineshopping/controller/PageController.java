ee19e69057b709a19d627f3bc341b12ff4c27fdc
sjx <jiaxinsun0715@gmail.com>
2018-10-16 14:44:07 -0400

    successful load main page

--- myonlineshopping/src/main/java/net/sjx/myonlineshopping/controller/PageController.java
+++ myonlineshopping/src/main/java/net/sjx/myonlineshopping/controller/PageController.java
@@ -1,7 +1,10 @@
 package net.sjx.myonlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,4 +17,21 @@ public class PageController {
 		return mv;
 	}
 
+//	@RequestMapping(value= "/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting==null)
+//			greeting="kathy";
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+//	@RequestMapping(value= "/test/{greeting}")
+//	public ModelAndView test(@PathVariable(value="greeting", required=false)String greeting) {
+//		if(greeting==null)
+//			greeting="kathy";
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
 }
